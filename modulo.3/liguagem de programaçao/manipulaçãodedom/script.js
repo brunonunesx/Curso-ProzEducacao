0a9c30a7ac47351195a8f5b37e58f06a5cad9865
Bruno Nunes <166269531+brunonunesx@users.noreply.github.com>
2025-08-11 23:56:19 -0300

    Módulo 3 do curso da proz professora Barbara
    curso de liguagem de programação da proz educação.
    atv feitas

---
+++ modulo.3/liguagem de programaçao/manipulaçãodedom/script.js
@@ -0,0 +1,5 @@
+function alterarConteudo(){
+     document.getElementById("titulo").innerText = "Novo Título";
+     document.getElementById("mensagem").innerHTML = "A mensagem foi alterado";
+     document.getElementById("botao").innerHTML = "Clique novamente";
+}
